3f9b82fa9839ec677915304632d653b87fc5bf8e
brokenerk <e_ramosdiaz_@outlook.com>
2023-05-15 17:04:49 -0600

    fix api backend prod url axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '.././store/index.js';
 
 let config = {
-  baseURL: process.env.VUE_APP_API_URL + "/e_commerce",
+  baseURL: "https://e-commerce-api.up.railway.app/e_commerce",
   headers: { 
     'Cache-Control': 'no-cache, must-revalidate', 
     'Pragma': 'no-cache', 
